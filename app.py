5d0e2cfd8647eff52dca2c6ae86f1c88305a00ab
Adrian Sanjuan <adriansanjuanes@gmail.com>
2024-12-14 17:55:46 +0100

    Add date aggregations to no1 functions and general stats

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 
+
 from src.data_processing import create_clean_dataframe
 from src.stats import get_most_listened_artists, get_most_listened_songs, get_most_no1_artists, get_most_no1_songs
 
@@ -11,8 +12,6 @@ from configurations.streamlit_config import table_layout
 import os
 
 DARK_GREEN = '#022100'
-
-
 
 # data_files = [f for f in os.listdir('data') if f.endswith('.json')]
 # df = create_clean_dataframe(data_files)
@@ -27,6 +26,25 @@ if uploaded_files:
     data_files = [f for f in uploaded_files]
     df = create_clean_dataframe(data_files)
 
+    st.markdown("---")
+
+    h1,h2,h3 = st.columns((1,1,1))
+
+    
+    # Total Minutes Played
+    h1.markdown(f"### :musical_note: **Total Minutes Played**")
+    h1.write(f"**{df['minutes'].sum():,.0f} minutes**")
+
+    # Unique Artists Played
+    h2.markdown(f"### :star: **Unique Artists Played**")
+    h2.write(f"**{df['artist'].nunique()} artists**")
+
+    # Unique Songs Played
+    h3.markdown(f"### :headphones: **Unique Songs Played**")
+    h3.write(f"**{df['track'].nunique()} songs**")
+
+    st.markdown("---")
+
     c1,c2 = st.columns((1,1))
 
     #---------- Artist Tables ----------
@@ -34,6 +52,15 @@ if uploaded_files:
     c1.subheader('Most listened artists')
 
     df_artists = get_most_listened_artists(df, mode='minutes')
+    df_artists_no1_days = get_most_no1_artists(df)
+    df_artists_no1_weeks = get_most_no1_artists(df, agg='week')
+    df_artists_no1_months = get_most_no1_artists(df, agg='month')   
+    df_artists = df_artists.merge(df_artists_no1_days, on='artist', how='left')
+    df_artists = df_artists.merge(df_artists_no1_weeks, on='artist', how='left')
+    df_artists = df_artists.merge(df_artists_no1_months, on='artist', how='left')
+    df_artists = df_artists.fillna(0).reset_index(drop=False)
+    df_artists['index'] = df_artists['index'] + 1
+
     fig_a = go.Figure(data=[go.Table(
         header=dict(values=list(df_artists.columns),
                     fill_color=DARK_GREEN,
@@ -45,26 +72,24 @@ if uploaded_files:
     ])
     fig_a.update_layout(table_layout)
 
-    df_artists_no1 = get_most_no1_artists(df)
-    fig_a_1 = go.Figure(data=[go.Table(
-        header=dict(values=list(df_artists_no1.columns),
-                    fill_color=DARK_GREEN,
-                    font=dict(size=12, color = 'white'),
-                    align='left'),
-        cells=dict(values=df_artists_no1.transpose().values.tolist(),
-                font=dict(size=12, color = 'black'),
-                align='left'))
-    ])
-    fig_a_1.update_layout(table_layout)
+    
 
     c1.plotly_chart(fig_a, use_container_width=True, key='table_artists')
-    c1.plotly_chart(fig_a_1, use_container_width=True, key='table_artists_no1')
 
     #---------- Song Tables ----------
 
     c2.subheader('Most listened songs')
 
     df_songs = get_most_listened_songs(df, mode='minutes')
+    df_songs_no1_days = get_most_no1_songs(df)
+    df_songs_no1_weeks = get_most_no1_songs(df, agg='week')
+    df_songs_no1_months = get_most_no1_songs(df, agg='month')   
+    df_songs = df_songs.merge(df_songs_no1_days, on=['track','artist'], how='left')
+    df_songs = df_songs.merge(df_songs_no1_weeks, on=['track','artist'], how='left')
+    df_songs = df_songs.merge(df_songs_no1_months, on=['track','artist'], how='left')
+    df_songs = df_songs.fillna(0).reset_index(drop=False)
+    df_songs['index'] = df_songs['index'] + 1
+
     fig_s = go.Figure(data=[go.Table(
         header=dict(values=list(df_songs.columns),
                     fill_color=DARK_GREEN,
@@ -76,25 +101,18 @@ if uploaded_files:
     ])
     fig_s.update_layout(table_layout)
 
-    df_songs_no1 = get_most_no1_songs(df)
-    fig_s_1 = go.Figure(data=[go.Table(
-        header=dict(values=list(df_songs_no1.columns),
-                    fill_color=DARK_GREEN,
-                    font=dict(size=12, color = 'white'),
-                    align='left'),
-        cells=dict(values=df_songs_no1.transpose().values.tolist(),
-                font=dict(size=12, color = 'black'),
-                align='left'))
-    ])
-    fig_s_1.update_layout(table_layout)
+
+    
+    
 
     c2.plotly_chart(fig_s, use_container_width=True, key='table_songs')
-    c2.plotly_chart(fig_s_1, use_container_width=True, key='table_songs_no1')
-
+    # with c2:
+    #     AgGrid(df_songs, sortable=True)
+    st.markdown("---")
     #---------- Weekly Evolution Graph ----------
 
-    d1,d2,d3 = st.columns((0.3,0.3,0.5))
-
+    d1,d2,d3,d4,d5 = st.columns((0.3,0.3,0.1,0.3,0.3))
+    
     # Artist Search Input
     selected_artist = d1.selectbox(
         "Write/Select Artist Name",
@@ -110,7 +128,7 @@ if uploaded_files:
 
     # Song Search Input
     selected_song = d2.selectbox(
-        "Write/Select Song Name (Filtered by Artist)",
+        "Write/Select Song Name",
         options=[""] + list(filtered_songs),
         format_func=lambda x: "" if x == "" else x,
     )
@@ -122,20 +140,30 @@ if uploaded_files:
     else:
         filtered_df = pd.DataFrame()
 
+    if selected_artist:
+        # Total Minutes Played
+        d4.markdown(f":musical_note: **Total Minutes Played**")
+        d4.write(f"**{filtered_df['minutes'].sum():,.0f} minutes**")
+
+        # Unique Songs Played
+        d5.markdown(f":headphones: **Unique Songs Played**")
+        d5.write(f"**{filtered_df['track'].nunique()} songs**")
+
+
     if not filtered_df.empty:
 
-        grouped_df = filtered_df.groupby(["calendar_week"]).agg({"minutes": "sum"}).sort_values("calendar_week").reset_index()
+        grouped_df = filtered_df.groupby(["week"]).agg({"minutes": "sum"}).sort_values("week").reset_index()
 
-        all_weeks = pd.period_range(start=grouped_df['calendar_week'].min(), end=grouped_df['calendar_week'].max(), freq='W')
-        all_weeks_df = pd.DataFrame(all_weeks, columns=['calendar_week'])
+        all_weeks = pd.period_range(start=grouped_df['week'].min(), end=grouped_df['week'].max(), freq='W')
+        all_weeks_df = pd.DataFrame(all_weeks, columns=['week'])
 
-        grouped_df = all_weeks_df.merge(grouped_df, on='calendar_week', how='left')
+        grouped_df = all_weeks_df.merge(grouped_df, on='week', how='left')
         grouped_df["minutes"] = grouped_df["minutes"].fillna(0)
-        grouped_df["calendar_week"] = grouped_df["calendar_week"].astype(str)
+        grouped_df["week"] = grouped_df["week"].astype(str)
 
         fig = px.line(
             grouped_df,
-            x="calendar_week",
+            x="week",
             y="minutes",
             title=f"Streamed Minutes per Week for {selected_artist} - {selected_song if selected_song else 'All Songs'}",
             markers=True,
@@ -144,5 +172,9 @@ if uploaded_files:
             xaxis_title="Calendar Week",
             yaxis_title="Streamed Minutes",
             template="plotly_white",
+            xaxis=dict(
+                tickangle=-45,  # Rotate the x-axis labels to avoid overlap
+                tickfont=dict(size=10),  # Adjust the font size for the x-axis labels
+            ),
         )
         st.plotly_chart(fig, use_container_width=True)
