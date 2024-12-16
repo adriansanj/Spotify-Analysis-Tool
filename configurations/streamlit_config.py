3bd57555cf290e8a3a4872d111d5914dd005e5cc
Adrian Sanjuan <adriansanjuanes@gmail.com>
2024-12-16 17:14:29 +0100

    Refactor streamlit configuration to define color and font settings for tables

--- configurations/streamlit_config.py
+++ configurations/streamlit_config.py
@@ -1,8 +1,13 @@
+# Colors
+DARK_GREEN = 'rgba(0, 21, 3, 1)'
+DARK_GREEN_TRANSPARENT = 'rgba(0, 21, 3, 0.2)'
+
+# Tables
 table_layout = dict(
     margin=dict(t=0, b=0, l=0, r=0),
     height=200,
 )
 
-
-DARK_GREEN = 'rgba(0, 21, 3, 1)'
-DARK_GREEN_TRANSPARENT = 'rgba(0, 21, 3, 0.2)'
+# Fonts
+font_table_header = dict(size=12, color = 'white')
+font_table_rows = dict(size=12, color = 'black')
