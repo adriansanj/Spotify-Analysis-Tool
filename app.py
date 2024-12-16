fd468e65f76bd94d79f3a244cc9063fefc2b3a8c
Adrian Sanjuan <adriansanjuanes@gmail.com>
2024-12-16 17:15:32 +0100

    Add functions to generate artist and song tables with aggregated statistics

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import plotly.express as px
 
 
 from src.data_processing import create_clean_dataframe
-from src.stats import get_most_listened_artists, get_most_listened_songs, get_most_no1_artists, get_most_no1_songs
+from src.stats import get_most_listened_artist_table, get_most_listened_songs_table
 
-from configurations.streamlit_config import table_layout, DARK_GREEN, DARK_GREEN_TRANSPARENT
+from configurations.streamlit_config import *
 import os
 
 # data_files = [f for f in os.listdir('data') if f.endswith('.json')]
@@ -25,7 +25,7 @@ if uploaded_files:
     df = create_clean_dataframe(data_files)
 
     
-
+    # ------ Time Filter ------
     st.write("### Filter Data by Date")
     a1,a2,a3 = st.columns((0.15,0.15,1))
     
@@ -36,13 +36,14 @@ if uploaded_files:
     start_date = a1.date_input("Start date:", value=min_time.to_pydatetime().date(), min_value=min_time.to_pydatetime().date(), max_value=max_time.to_pydatetime().date())
     end_date = a2.date_input("End date:", value=max_time.to_pydatetime().date(), min_value=min_time.to_pydatetime().date(), max_value=max_time.to_pydatetime().date())
 
-    df = df[(df["timestamp"] >= pd.Timestamp(start_date, tz="UTC")) & 
-                     (df["timestamp"] <= pd.Timestamp(end_date, tz="UTC"))]
-    
+    # Filter the dataframe based on the selected time range
+    df = df[(df["timestamp"] >= pd.Timestamp(start_date, tz="UTC")) & (df["timestamp"] <= pd.Timestamp(end_date, tz="UTC"))]
+    #--------------------------
+
     st.markdown("---")
 
+    #---------- General Statistics ----------
     h1,h2,h3 = st.columns((1,1,1))
-
     
     # Total Minutes Played
     h1.markdown(f"### :musical_note: **Total Minutes Played**")
@@ -55,72 +56,53 @@ if uploaded_files:
     # Unique Songs Played
     h3.markdown(f"### :headphones: **Unique Songs Played**")
     h3.write(f"**{df['track'].nunique()} songs**")
+    #---------------------------------------
 
     st.markdown("---")
 
     c1,c2 = st.columns((1,1))
 
-    #---------- Artist Tables ----------
-
+    #---------- Artist Table ----------
     c1.subheader('Most listened artists')
 
-    df_artists = get_most_listened_artists(df, mode='minutes')
-    df_artists_no1_days = get_most_no1_artists(df)
-    df_artists_no1_weeks = get_most_no1_artists(df, agg='week')
-    df_artists_no1_months = get_most_no1_artists(df, agg='month')   
-    df_artists = df_artists.merge(df_artists_no1_days, on='artist', how='left')
-    df_artists = df_artists.merge(df_artists_no1_weeks, on='artist', how='left')
-    df_artists = df_artists.merge(df_artists_no1_months, on='artist', how='left')
-    df_artists = df_artists.fillna(0).reset_index(drop=False)
-    df_artists['index'] = df_artists['index'] + 1
+    df_artists = get_most_listened_artist_table(df)
 
+    # Create figure
     fig_a = go.Figure(data=[go.Table(
         header=dict(values=list(df_artists.columns),
                     fill_color=DARK_GREEN,
-                    font=dict(size=12, color = 'white'),
+                    font=font_table_header,
                     align='left'),
         cells=dict(values=df_artists.transpose().values.tolist(),
-                font=dict(size=12, color = 'black'),
+                font=font_table_rows,
                 align='left'))
     ])
     fig_a.update_layout(table_layout)
 
-    
-
+    # Display the table in Streamlit
     c1.plotly_chart(fig_a, use_container_width=True, key='table_artists')
 
     #---------- Song Tables ----------
-
     c2.subheader('Most listened songs')
 
-    df_songs = get_most_listened_songs(df, mode='minutes')
-    df_songs_no1_days = get_most_no1_songs(df)
-    df_songs_no1_weeks = get_most_no1_songs(df, agg='week')
-    df_songs_no1_months = get_most_no1_songs(df, agg='month')   
-    df_songs = df_songs.merge(df_songs_no1_days, on=['track','artist'], how='left')
-    df_songs = df_songs.merge(df_songs_no1_weeks, on=['track','artist'], how='left')
-    df_songs = df_songs.merge(df_songs_no1_months, on=['track','artist'], how='left')
-    df_songs = df_songs.fillna(0).reset_index(drop=False)
-    df_songs['index'] = df_songs['index'] + 1
+    df_songs = get_most_listened_songs_table(df)
 
+    # Create figure
     fig_s = go.Figure(data=[go.Table(
         header=dict(values=list(df_songs.columns),
                     fill_color=DARK_GREEN,
-                    font=dict(size=12, color = 'white'),
+                    font=font_table_header,
                     align='left'),
         cells=dict(values=df_songs.transpose().values.tolist(),
-                font=dict(size=12, color = 'black'),
+                font=font_table_rows,
                 align='left'))
     ])
     fig_s.update_layout(table_layout)
 
 
-    
-    
-
+    # Display the table in Streamlit
     c2.plotly_chart(fig_s, use_container_width=True, key='table_songs')
-    # with c2:
-    #     AgGrid(df_songs, sortable=True)
+   
     st.markdown("---")
     #---------- Weekly Evolution Graph ----------
 
